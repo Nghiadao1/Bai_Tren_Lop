ee94c3186bc52f5d25896da6403efb59026f1394
Nghiadao1 <105330547+Nghiadao1@users.noreply.github.com>
2023-02-19 17:53:17 +0700

    het roi

---
+++ WebAuth/WebAuth/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAuth.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Title { get; set; }
+        public string UrlImage { get; set; }
+        public string Detail { get; set; }
+        public decimal Price { get; set; }
+    }
+}
